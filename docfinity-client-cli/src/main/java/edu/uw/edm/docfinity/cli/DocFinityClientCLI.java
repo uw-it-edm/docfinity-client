e623905d74c514a1924858d195f24ba824777474
Federico Silva Armas <farmas@uw.edu>
2021-04-07 10:04:09 -0700

    Adds ability to CLI to read json metadata from a file.

--- docfinity-client-cli/src/main/java/edu/uw/edm/docfinity/cli/DocFinityClientCLI.java
+++ docfinity-client-cli/src/main/java/edu/uw/edm/docfinity/cli/DocFinityClientCLI.java
@@ -12,6 +12,8 @@ import edu.uw.edm.docfinity.DocFinityDocumentField;
 import edu.uw.edm.docfinity.DocFinityServiceImpl;
 import java.io.File;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 import org.slf4j.LoggerFactory;
@@ -50,11 +52,15 @@ public class DocFinityClientCLI {
             description = "Path of file to upload, or 'test' to upload a sample file")
     String filePath;
 
-    // TODO: Add a parameter to read metadata from file.
     @Parameter(
             names = {"--metadataJson", "-j"},
             description = "Json array with metadata values to index the file.")
     String metadataJson = "[]";
+
+    @Parameter(
+            names = {"--metadataFile", "-m"},
+            description = "File path with with metadata values as Json to use when indexing.")
+    String metadataFilePath;
 
     @Parameter(names = "--trace", description = "Enable request tracing to console.")
     boolean trace;
@@ -80,27 +86,14 @@ public class DocFinityClientCLI {
             return;
         }
 
-        if (!cli.trace) {
-            // A console appender is defined in 'docfinity-client-cli/src/main/resources/logback.xml', if
-            // user did not enable tracing need to detach it.
-            String serviceImplTypeName = DocFinityServiceImpl.class.getCanonicalName();
-            Logger clientLogger = (Logger) LoggerFactory.getLogger(serviceImplTypeName);
-            clientLogger.detachAppender("Console");
-        }
-
         // Load the file to upload.
-        Preconditions.checkNotNull(cli.filePath, "file is required.");
-        File file = new File(cli.filePath);
-
-        if (cli.filePath.equals("test")) {
-            URL resource = DocFinityClientCLI.class.getClassLoader().getResource("test-file.pdf");
-            file = new File(resource.toURI());
-        }
+        File file = getFile(cli);
 
         // Load metadata from json
-        ObjectMapper mapper = new ObjectMapper();
-        List<DocFinityDocumentField> metadata =
-                Arrays.asList(mapper.readValue(cli.metadataJson, DocFinityDocumentField[].class));
+        List<DocFinityDocumentField> metadata = loadMetadata(cli);
+
+        // Setup logging for request/responses.
+        setupRequestTracing(cli);
 
         // Run the client.
         cliLogger.info("Starting");
@@ -109,5 +102,37 @@ public class DocFinityClientCLI {
                 new CreateDocumentArgs(file, cli.category, cli.documentType).withMetadata(metadata);
         CreateDocumentResult result = client.createDocument(args);
         cliLogger.info("Result: {}", result.getDocumentId());
+    }
+
+    private static void setupRequestTracing(DocFinityClientCLI cli) {
+        if (!cli.trace) {
+            // A console appender is defined in 'docfinity-client-cli/src/main/resources/logback.xml', if
+            // user did not enable tracing need to detach it.
+            String serviceImplTypeName = DocFinityServiceImpl.class.getCanonicalName();
+            Logger clientLogger = (Logger) LoggerFactory.getLogger(serviceImplTypeName);
+            clientLogger.detachAppender("Console");
+        }
+    }
+
+    private static File getFile(DocFinityClientCLI cli) throws Exception {
+        Preconditions.checkNotNull(cli.filePath, "file is required.");
+        File file = new File(cli.filePath);
+
+        if (cli.filePath.equals("test")) {
+            URL resource = DocFinityClientCLI.class.getClassLoader().getResource("test-file.pdf");
+            file = new File(resource.toURI());
+        }
+        return file;
+    }
+
+    private static List<DocFinityDocumentField> loadMetadata(DocFinityClientCLI cli)
+            throws Exception {
+        String metadataJson = cli.metadataJson;
+        if (cli.metadataFilePath != null) {
+            metadataJson = new String(Files.readAllBytes(Paths.get(cli.metadataFilePath)));
+        }
+
+        ObjectMapper mapper = new ObjectMapper();
+        return Arrays.asList(mapper.readValue(metadataJson, DocFinityDocumentField[].class));
     }
 }
